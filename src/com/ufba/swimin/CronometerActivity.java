207937a4ce4c4efe1b83297348bfa90366bb55e9
Helder Santana <contato@heldersantana.net>
2013-11-21 20:16:51 -0300

    Activity das telas de cronômetro, estatísticas e lista de usuários

---
+++ src/com/ufba/swimin/CronometerActivity.java
@@ -0,0 +1,23 @@
+package com.ufba.swimin;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CronometerActivity  extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cronometer);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+}
+
