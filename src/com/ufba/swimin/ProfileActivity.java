3165c4b7f90b44978f866338453c090d541d65ec
thalitasa <thalitasa@dcc.ufba.br>
2013-12-10 17:56:33 -0300

    Funcionalidade da Tela de Treino

--- src/com/ufba/swimin/ProfileActivity.java
+++ src/com/ufba/swimin/ProfileActivity.java
@@ -1,15 +1,44 @@
 package com.ufba.swimin;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ProfileActivity extends Activity {
+    Button btStatistics, btPremios;
+    TextView txtNome;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.profile);
+
+        btStatistics = (Button) findViewById(R.id.button);
+        btPremios = (Button) findViewById(R.id.button2);
+        txtNome = (TextView) findViewById(R.id.textView2);
+
+        btStatistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent i = new Intent(getApplicationContext(), StatisticsActivity.class);
+                //i.putExtra("meters", meters.getText().toString());
+                startActivity(i);
+            }
+        });
+
+        btPremios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //txtNome.setText("Hi there, Thalita!");
+                Intent i = new Intent(getApplicationContext(), Premios.class);
+                //i.putExtra("meters", meters.getText().toString());
+                startActivity(i);
+            }
+        });
 	}
 
 	@Override
