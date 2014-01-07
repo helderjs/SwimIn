8c0d314727ad1b107378f28429935e4584b4f3ec
democlitos <skhacker2@hotmail.com>
2014-01-07 19:59:26 -0300

    Código para criação de tabelas finalizado; persistência para tela de prêmios concluída.

--- src/com/ufba/swimin/HomeCoachActivity.java
+++ src/com/ufba/swimin/HomeCoachActivity.java
@@ -1,15 +1,21 @@
 package com.ufba.swimin;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageButton;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.Cursor;
 
 public class HomeCoachActivity extends Activity {
 
+	SQLiteDatabase bancoDados = null;
+	Cursor cursor;
+	
 	ImageButton btPerfil, btUsuarios, btTreinos, btRanking;
 	Context context = this;
 	
@@ -19,6 +25,9 @@ public class HomeCoachActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home_coach);
+		
+		abreOuCriaBanco();
+		//gravarRegistro();
 		
 		btPerfil = (ImageButton) findViewById(R.id.btPerfil);
 		btUsuarios = (ImageButton) findViewById(R.id.btUsuarios);
@@ -68,9 +77,140 @@ public class HomeCoachActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
+	//*******************	BANCO	*******************		
+	
+	public void abreOuCriaBanco() {
+		try{
+			//Cria ou abre o banco
+			bancoDados = openOrCreateDatabase("bancoSwing", MODE_WORLD_READABLE, null);
+			
+			//bancoDados.execSQL("DROP TABLE IF EXISTS premios");
+			
+			String sql = "CREATE TABLE IF NOT EXISTS pessoas "
+					+ "(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, data_nasc TEXT, endereco TEXT)";
+			bancoDados.execSQL(sql);
+			
+			sql = "CREATE TABLE IF NOT EXISTS atletas "
+					+ "(id INTEGER PRIMARY KEY REFERENCES pessoas (id), "
+					+ "peso REAL, altura REAL)";
+			bancoDados.execSQL(sql);
+			
+			sql = "CREATE TABLE IF NOT EXISTS treinadores "
+					+ "(id INTEGER PRIMARY KEY REFERENCES pessoas(id), "
+					+ "numero_atletas INTEGER)";
+			bancoDados.execSQL(sql);
+			
+			sql = "CREATE TABLE IF NOT EXISTS atletas_treinadores "
+					+ "(id_atleta INTEGER PRIMARY KEY REFERENCES atletas(id), "
+					+ "id_treinador INTEGER, "
+					+ "FOREIGN KEY (id_treinador) REFERENCES treinadores(id))";
+			bancoDados.execSQL(sql);
+			
+			sql = "CREATE TABLE IF NOT EXISTS treinos "
+					+ "(id INTEGER PRIMARY KEY AUTOINCREMENT, id_treinador INTEGER, "
+					+ "id_atleta INTEGER, tipo_nado TEXT, metros REAL, tempo INTEGER, "
+					+ "FOREIGN KEY (id_treinador) REFERENCES treinadores(id), "
+					+ "FOREIGN KEY (id_atleta) REFERENCES atletas(id))";
+			bancoDados.execSQL(sql);
+			
+			sql = "CREATE TABLE IF NOT EXISTS premios "
+					+ "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
+					+ "id_atleta INTEGER, nome TEXT, tipo INTEGER, "
+					+ "FOREIGN KEY (id_atleta) REFERENCES atletas(id))";
+			bancoDados.execSQL(sql);
+		}catch(Exception erro){
+			exibirMensagem("Erro banco", "Erro ao abrir ou criar banco: " + erro.getMessage());
+		}
+	}
+	
+	public void fechaBanco(){
+		try{
+			bancoDados.close();
+		}catch(Exception erro){
+			exibirMensagem("Erro Banco", "Erro ao fechar o banco " + erro.getMessage());
+		}
+	}
+	
+	private boolean buscarDados(){
+		try{
+			cursor = bancoDados.query("pessoas", 
+					new String [] {"nome","dataNasc","endereco"}, 
+					null,//selection,
+					null,//selectionArgs, 
+					null,//groupBy, 
+					null,//having,
+					null,//orderBy,
+					null);//Limite de registros retornados
+			
+			int numeroRegistros = cursor.getCount();
+			if(numeroRegistros!=0){
+				cursor.moveToFirst();
+				return true;
+			}else
+				return false;	
+		}catch(Exception erro){
+			exibirMensagem("Erro banco.", "Erro ao buscar dados no banco: " + erro.getMessage());
+			return false;
+		}
+	}
+	
+	public void mostraProximoRegistro(){
+		try{
+			cursor.moveToNext();
+			mostrarDados();
+		}catch(Exception erro){
+			exibirMensagem("Erro","Erro ao acessar próximo registro: "+erro.getMessage());
+		}
+	}
+	
+	public void mostraRegistroAnterior(){
+		try{
+			cursor.moveToPrevious();
+			mostrarDados();
+		}catch(Exception erro){
+			exibirMensagem("Erro","Erro ao acessar registro anterior: "+erro.getMessage());
+		}
+	}
+	
+	public void gravarRegistro(){
+		try{
+			/*String sql = "INSERT INTO pessoas (nome, data_nasc, endereco) values "
+					+ "('Manoel Neto', '14/07/1977', 'Rua Rochael Tantan n 38 - Salvador BA'), "
+					+ "('Thalita Andrade', '09/11/1988', 'Rua Itambém Ticotico - Salvador BA'), "
+					+ "('Helder Carvalho', '11/12/1995', 'Rua Circo Pegafogo - Salvador BA'),"
+					+ "('Lucas Augusto', '30/03/1956', 'Rua Vandré Datena - São Paulo SP')";*/
+			String sql = "INSERT INTO premios (id_atleta, nome, tipo) values "
+					+ "(1, 'Olimpiadas 2006', 0), "
+					+ "(1, 'Olimpiadas 2007', 2), "
+					+ "(1, 'Olimpiadas 2008', 2), "
+					+ "(1, 'Rio 2011', 1)";
+			bancoDados.execSQL(sql);
+			
+			exibirMensagem("Banco", "Dados gravados com sucesso!");
+		}catch(Exception erro){
+			exibirMensagem("Erro","Erro ao gravar dados: " + erro.getMessage());
+		}
+	}
+	
+	//*******************	FIM BANCO	*******************
+	
+	//*******************	INTERFACE	*******************
+	
+	public void exibirMensagem(String titulo, String texto){
+		AlertDialog.Builder mensagem = new AlertDialog.Builder(HomeCoachActivity.this);
+		mensagem.setTitle(titulo);
+		mensagem.setMessage(texto);
+		mensagem.setNeutralButton("Ok", null);
+		mensagem.show();
+	}
+	
+	//*******************	FIM INTERFACE	*******************
+	
+	public void mostrarDados(){
+		
+	}
 }
--- src/com/ufba/swimin/Premios.java
+++ src/com/ufba/swimin/Premios.java
@@ -3,6 +3,8 @@ package com.ufba.swimin;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -18,14 +20,63 @@ public class Premios extends Activity {
 	
 	TableLayout tbMedalhas;
 	Button btAdicionar;
+	TableRow tbRow = null;
+	
+	SQLiteDatabase bancoDados = null;
+	Cursor cursor;
+	int count = 0;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.premios);
 		
+		bancoDados = openOrCreateDatabase("bancoSwing", MODE_WORLD_READABLE, null);
+		
 		tbMedalhas = (TableLayout) findViewById(R.id.tbMedalhas);
 		btAdicionar = (Button) findViewById(R.id.btAdicionar);
+		tbRow = (TableRow) findViewById(R.id.tbRow1);
+		
+		ImageView ivMedalha = (ImageView) findViewById(R.id.ivMedalha1);
+		ivMedalha.setVisibility(View.GONE);
+		
+		if(buscarDados()){
+			cursor.moveToFirst();
+			count=cursor.getCount();
+			for(int i = 0; i<cursor.getCount(); i++){
+				
+				if(i==7){
+					tbRow = (TableRow) findViewById(R.id.tbRow2);
+				}
+				if(i==15){
+					tbRow = (TableRow) findViewById(R.id.tbRow3);
+				}
+				if(i==22){
+					tbRow = (TableRow) findViewById(R.id.tbRow4);
+				}
+				if(i==29){
+					tbRow = (TableRow) findViewById(R.id.tbRow5);
+				}
+				
+				final ImageView medalha = new ImageView(Premios.this);
+				
+				switch(cursor.getInt(cursor.getColumnIndex("tipo"))){
+				case 0: medalha.setImageResource(R.drawable.silver_medal);
+					medalha.setLayoutParams(ivMedalha.getLayoutParams());
+					tbRow.addView(medalha);
+					break;
+				case 1: medalha.setImageResource(R.drawable.bronze_medal);
+					medalha.setLayoutParams(ivMedalha.getLayoutParams());
+					tbRow.addView(medalha);
+					break;
+				case 2: medalha.setImageResource(R.drawable.gold_medal);
+					medalha.setLayoutParams(ivMedalha.getLayoutParams());
+					tbRow.addView(medalha);
+					break;
+				}
+				cursor.moveToNext();				
+			}
+		}
 		
 		btAdicionar.setOnClickListener(new View.OnClickListener() {
 			
@@ -39,7 +90,24 @@ public class Premios extends Activity {
 					return;
 				}
 				
-				TableRow tbRow = (TableRow) findViewById(R.id.tbRow1);
+				if(count>34){
+					exibirMensagem("Atenção","Você atingiu o número máximo de premiações.");
+					return;
+				}
+				
+				if(count==7){
+					tbRow = (TableRow) findViewById(R.id.tbRow2);
+				}
+				if(count==14){
+					tbRow = (TableRow) findViewById(R.id.tbRow3);
+				}
+				if(count==21){
+					tbRow = (TableRow) findViewById(R.id.tbRow4);
+				}
+				if(count==28){
+					tbRow = (TableRow) findViewById(R.id.tbRow5);
+				}
+				
 				ImageView ivMedalha = (ImageView) findViewById(R.id.ivMedalha1);
 				
 				final ImageView medalha = new ImageView(Premios.this);
@@ -52,39 +120,69 @@ public class Premios extends Activity {
 				case 0: medalha.setImageResource(R.drawable.silver_medal);
 					medalha.setLayoutParams(ivMedalha.getLayoutParams());
 					tbRow.addView(medalha);
+					count++;
+					gravarMedalhaBanco(1,etNome.getText().toString(),rbSelecionado);
 					break;
 				case 1: medalha.setImageResource(R.drawable.bronze_medal);
 					medalha.setLayoutParams(ivMedalha.getLayoutParams());
 					tbRow.addView(medalha);
+					count++;
+					gravarMedalhaBanco(1,etNome.getText().toString(),rbSelecionado);
 					break;
 				case 2: medalha.setImageResource(R.drawable.gold_medal);
 					medalha.setLayoutParams(ivMedalha.getLayoutParams());
 					tbRow.addView(medalha);
+					count++;
+					gravarMedalhaBanco(1,etNome.getText().toString(),rbSelecionado);
 					break;
 				default: exibirMensagem("Atenção", "É preciso selecionar o tipo de medalha");
 				}
 			}
 		});
-		
-		/*TableRow tbRow = (TableRow) findViewById(R.id.tbRow1);
-		
-		ImageView ivMedalha = (ImageView) findViewById(R.id.ivMedalha1);
-		
-		final ImageView medalha = new ImageView(this);
-		medalha.setImageResource(R.drawable.silver_medal);
-		
-		medalha.setLayoutParams(ivMedalha.getLayoutParams());
-		
-		tbRow.addView(medalha);*/
 	}
 
+	private boolean buscarDados(){
+		try{
+			cursor = bancoDados.query("premios", 
+					new String [] {"id_atleta","nome","tipo"}, 
+					"id_atleta = 1",//selection,
+					null,//selectionArgs, 
+					null,//groupBy, 
+					null,//having,
+					null,//orderBy,
+					null);//Limite de registros retornados
+			
+			int numeroRegistros = cursor.getCount();
+			if(numeroRegistros!=0){
+				cursor.moveToFirst();
+				return true;
+			}else
+				return false;	
+		}catch(Exception erro){
+			exibirMensagem("Erro banco.", "Erro ao buscar dados no banco: " + erro.getMessage());
+			return false;
+		}
+	}
+	
+	public void gravarMedalhaBanco(int id_atleta, String nome, int tipo){
+		try{
+			String sql = "INSERT INTO premios (id_atleta, nome, tipo) values "
+					+ "("+id_atleta+",'"+nome+"',"+tipo+")";
+			bancoDados.execSQL(sql);
+			
+			//exibirMensagem("Premiação", "Medalha adicionada com sucesso!");
+		}catch(Exception erro){
+			exibirMensagem("Erro","Erro ao gravar dados: " + erro.getMessage());
+		}
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-
+	
 	public void exibirMensagem(String titulo, String texto){
 		AlertDialog.Builder mensagem = new AlertDialog.Builder(Premios.this);
 		mensagem.setTitle(titulo);
