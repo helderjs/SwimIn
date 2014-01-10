f184e9864d98e4955e0412e1969a8fa3421faa3a
democlitos <skhacker2@hotmail.com>
2014-01-10 18:13:35 -0300

    Atualização da tela de prêmios: agora exibe medalhas por atleta.

--- src/com/ufba/swimin/Premios.java
+++ src/com/ufba/swimin/Premios.java
@@ -1,5 +1,7 @@
 package com.ufba.swimin;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -8,10 +10,15 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 
@@ -22,30 +29,192 @@ public class Premios extends Activity {
 	TableLayout tbMedalhas;
 	Button btAdicionar;
 	TableRow tbRow = null;
+	Spinner spnAtletas;
+	ImageView ivMedalha;
+	
+	ArrayList <ImageView> ivArray;
+	
+	ArrayList <String> spnArray;
+	ArrayList <Integer> spnArrayId;
+	SpinnerAdapter adapter;
 	
 	SQLiteDatabase bancoDados = null;
 	Cursor cursor;
 	int count = 0;
+	
+	int index = 0; //indice do atleta selecionado
+	
+	String id_treinador = "2";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.premios);
 		
+		//Abre banco
 		bancoDados = openOrCreateDatabase("bancoSwing", MODE_WORLD_READABLE, null);
 		
 		tbMedalhas = (TableLayout) findViewById(R.id.tbMedalhas);
 		btAdicionar = (Button) findViewById(R.id.btAdicionar);
 		tbRow = (TableRow) findViewById(R.id.tbRow1);
+		spnAtletas = (Spinner) findViewById(R.id.spnAtleta);
+	 
+		spnArray = new ArrayList<String>();
+	    spnArrayId = new ArrayList<Integer>();
+	    
+	    ivArray = new ArrayList<ImageView>();
 		
 		//O ImageView ivMedalha1 serve para passamarmos seus atributos,
 		//como largura e altura, as outras medalhas - isso é feito com
 		//ivMedalha.getLayoutParams()
 		
-		ImageView ivMedalha = (ImageView) findViewById(R.id.ivMedalha1);
+		ivMedalha = (ImageView) findViewById(R.id.ivMedalha1);
 		ivMedalha.setVisibility(View.GONE);
 		
-		if(buscarDados()){
+		buscarDados("SELECT p.id, nome FROM pessoas AS p, atletas_treinadores AS a "
+				+ "WHERE p.id = a.id_atleta "
+				+ "AND a.id_treinador = "+id_treinador);
+		
+		//Carrega atletas no spinner
+		
+		if(cursor.getCount()>0){
+			do{
+				spnArray.add(cursor.getString(cursor.getColumnIndex("nome")));
+				spnArrayId.add(cursor.getInt(cursor.getColumnIndex("id")));
+			}while(cursor.moveToNext());
+			
+			adapter = new ArrayAdapter<String>(Premios.this, 
+					android.R.layout.simple_spinner_item, spnArray);
+			spnAtletas.setAdapter(adapter);
+			
+			//Carrega as medalhas do primeiro atleta
+			index = 0;			
+			//carregaMedalhasDoAtleta(spnArrayId.get(index));
+			
+			spnAtletas.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+	            @Override
+	            public void onItemSelected(AdapterView<?> arg0, View arg1,
+	                    int arg2, long arg3) {
+	                // TODO Auto-generated method stub
+	                Object item = arg0.getItemAtPosition(arg2);
+	                if (item!=null) {
+						int pos = arg2;
+		
+						index = pos;
+						while(ivArray.size()>0){
+							ivArray.get(0).setVisibility(View.GONE);
+							ivArray.remove(0);
+						}
+						
+						tbRow = (TableRow) findViewById(R.id.tbRow1);
+						
+						carregaMedalhasDoAtleta(spnArrayId.get(index));
+	                }
+
+	            }
+
+	            @Override
+	            public void onNothingSelected(AdapterView<?> arg0) {
+	                // TODO Auto-generated method stub
+
+	            }
+	        });
+						
+			btAdicionar.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View arg0) {
+					
+					EditText etNome = (EditText) findViewById(R.id.etNome);
+					
+					//Compara se o conteúdo do campo nome é igual a vazio
+					if(etNome.getText().toString().equals("")){
+						exibirMensagem("Atenção","Você deve dar um nome a premiação.");
+						return;
+					}
+					
+					//Retorna se o número de medalhas for maior que 34
+					
+					if(count>34){
+						exibirMensagem("Atenção","Você atingiu o número máximo de premiações.");
+						return;
+					}
+					
+					/*Caso existam 7 medalhas em uma mesma linha, as 
+					 * próximas serão adicionadas na linha logo abaixo
+					 * */
+					
+					//exibirMensagem("Count",Integer.toString(count));
+					
+					if(count==7){
+						tbRow = (TableRow) findViewById(R.id.tbRow2);
+					}
+					if(count==14){
+						tbRow = (TableRow) findViewById(R.id.tbRow3);
+					}
+					if(count==21){
+						tbRow = (TableRow) findViewById(R.id.tbRow4);
+					}
+					if(count==28){
+						tbRow = (TableRow) findViewById(R.id.tbRow5);
+					}
+					
+					final ImageView medalha = new ImageView(Premios.this);
+					
+					//A posição da nova medalha será a última posição + 1
+					cursor.moveToLast();
+					medalha.setTag(cursor.getPosition()+1);
+					
+					//Captura o índice do radioButton selecionado
+					
+					RadioGroup rgMedalha = (RadioGroup) findViewById(R.id.rgMedalha);
+					int rbId = rgMedalha.getCheckedRadioButtonId();
+					int rbSelecionado = rgMedalha.indexOfChild(rgMedalha.findViewById(rbId));
+					
+					switch(rbSelecionado){
+					case 0: medalha.setImageResource(R.drawable.silver_medal);
+						medalha.setLayoutParams(ivMedalha.getLayoutParams());
+						tbRow.addView(medalha);
+						count++;
+						gravarMedalhaBanco(spnArrayId.get(index),etNome.getText().toString(),rbSelecionado);
+						ivArray.add(medalha);
+						buscarDados(spnArrayId.get(index));
+						
+						eventoClicaMedalha(medalha, "Medalha de prata");
+						
+						break;
+					case 1: medalha.setImageResource(R.drawable.bronze_medal);
+						medalha.setLayoutParams(ivMedalha.getLayoutParams());
+						tbRow.addView(medalha);
+						count++;
+						gravarMedalhaBanco(spnArrayId.get(index),etNome.getText().toString(),rbSelecionado);
+						ivArray.add(medalha);
+						buscarDados(spnArrayId.get(index));
+						
+						eventoClicaMedalha(medalha, "Medalha de bronze");
+						
+						break;
+					case 2: medalha.setImageResource(R.drawable.gold_medal);
+						medalha.setLayoutParams(ivMedalha.getLayoutParams());
+						tbRow.addView(medalha);
+						count++;
+						gravarMedalhaBanco(spnArrayId.get(index),etNome.getText().toString(),rbSelecionado);
+						ivArray.add(medalha);
+						buscarDados(spnArrayId.get(index));
+						
+						eventoClicaMedalha(medalha, "Medalha de ouro");
+						
+						break;
+					default: exibirMensagem("Atenção", "É preciso selecionar o tipo de medalha");
+					}
+				}
+			});
+		}
+	}
+
+	private void carregaMedalhasDoAtleta(int id_atleta){
+		if(buscarDados(id_atleta)){
 			cursor.moveToFirst();	//Aponta para a primeira linha da tabela
 			count=cursor.getCount();	//Guarda o número atual de medalhas
 			
@@ -60,13 +229,13 @@ public class Premios extends Activity {
 				if(i==7){
 					tbRow = (TableRow) findViewById(R.id.tbRow2);
 				}
-				if(i==15){
+				if(i==14){
 					tbRow = (TableRow) findViewById(R.id.tbRow3);
 				}
-				if(i==22){
+				if(i==21){
 					tbRow = (TableRow) findViewById(R.id.tbRow4);
 				}
-				if(i==29){
+				if(i==28){
 					tbRow = (TableRow) findViewById(R.id.tbRow5);
 				}
 				
@@ -84,6 +253,7 @@ public class Premios extends Activity {
 				case 0: medalha.setImageResource(R.drawable.silver_medal);
 					medalha.setLayoutParams(ivMedalha.getLayoutParams());
 					tbRow.addView(medalha);
+					ivArray.add(medalha);
 					
 					eventoClicaMedalha(medalha,"Medalha de prata");
 					
@@ -91,6 +261,7 @@ public class Premios extends Activity {
 				case 1: medalha.setImageResource(R.drawable.bronze_medal);
 					medalha.setLayoutParams(ivMedalha.getLayoutParams());
 					tbRow.addView(medalha);
+					ivArray.add(medalha);
 					
 					eventoClicaMedalha(medalha,"Medalha de bronze");
 					
@@ -98,6 +269,7 @@ public class Premios extends Activity {
 				case 2: medalha.setImageResource(R.drawable.gold_medal);
 					medalha.setLayoutParams(ivMedalha.getLayoutParams());
 					tbRow.addView(medalha);
+					ivArray.add(medalha);
 					
 					eventoClicaMedalha(medalha,"Medalha de ouro");
 					
@@ -106,102 +278,32 @@ public class Premios extends Activity {
 				cursor.moveToNext();				
 			}
 		}
-		
-		btAdicionar.setOnClickListener(new View.OnClickListener() {
-			
-			@Override
-			public void onClick(View arg0) {
-				
-				EditText etNome = (EditText) findViewById(R.id.etNome);
-				
-				//Compara se o conteúdo do campo nome é igual a vazio
-				if(etNome.getText().toString().equals("")){
-					exibirMensagem("Atenção","Você deve dar um nome a premiação.");
-					return;
-				}
-				
-				//Retorna se o número de medalhas for maior que 34
-				
-				if(count>34){
-					exibirMensagem("Atenção","Você atingiu o número máximo de premiações.");
-					return;
-				}
-				
-				/*Caso existam 7 medalhas em uma mesma linha, as 
-				 * próximas serão adicionadas na linha logo abaixo
-				 * */
-				
-				if(count==7){
-					tbRow = (TableRow) findViewById(R.id.tbRow2);
-				}
-				if(count==14){
-					tbRow = (TableRow) findViewById(R.id.tbRow3);
-				}
-				if(count==21){
-					tbRow = (TableRow) findViewById(R.id.tbRow4);
-				}
-				if(count==28){
-					tbRow = (TableRow) findViewById(R.id.tbRow5);
-				}
-				
-				ImageView ivMedalha = (ImageView) findViewById(R.id.ivMedalha1);
-				
-				final ImageView medalha = new ImageView(Premios.this);
-				
-				//A posição da nova medalha será a última posição + 1
-				cursor.moveToLast();
-				medalha.setTag(cursor.getPosition()+1);
-				
-				//Captura o índice do radioButton selecionado
-				
-				RadioGroup rgMedalha = (RadioGroup) findViewById(R.id.rgMedalha);
-				int rbId = rgMedalha.getCheckedRadioButtonId();
-				int rbSelecionado = rgMedalha.indexOfChild(rgMedalha.findViewById(rbId));
-				
-				switch(rbSelecionado){
-				case 0: medalha.setImageResource(R.drawable.silver_medal);
-					medalha.setLayoutParams(ivMedalha.getLayoutParams());
-					tbRow.addView(medalha);
-					count++;
-					gravarMedalhaBanco(1,etNome.getText().toString(),rbSelecionado);
-					buscarDados();
-					
-					eventoClicaMedalha(medalha, "Medalha de prata");
-					
-					break;
-				case 1: medalha.setImageResource(R.drawable.bronze_medal);
-					medalha.setLayoutParams(ivMedalha.getLayoutParams());
-					tbRow.addView(medalha);
-					count++;
-					gravarMedalhaBanco(1,etNome.getText().toString(),rbSelecionado);
-					buscarDados();
-					
-					eventoClicaMedalha(medalha, "Medalha de bronze");
-					
-					break;
-				case 2: medalha.setImageResource(R.drawable.gold_medal);
-					medalha.setLayoutParams(ivMedalha.getLayoutParams());
-					tbRow.addView(medalha);
-					count++;
-					gravarMedalhaBanco(1,etNome.getText().toString(),rbSelecionado);
-					buscarDados();
-					
-					eventoClicaMedalha(medalha, "Medalha de ouro");
-					
-					break;
-				default: exibirMensagem("Atenção", "É preciso selecionar o tipo de medalha");
-				}
-			}
-		});
 	}
-
+	
+	
 	//Busca a tablea com todas as medalhas de determinado atleta
 	
-	private boolean buscarDados(){
+	private boolean buscarDados(String sql){
+    	try{
+    		cursor = bancoDados.rawQuery(sql, null);
+    		
+    		int numeroRegistros = cursor.getCount();
+			if(numeroRegistros!=0){
+				cursor.moveToFirst();
+				return true;
+			}else
+				return false;
+    	}catch(Exception erro){
+    		exibirMensagem("Erro banco.", "Erro ao buscar dados no banco: " + erro.getMessage());
+    		return false;
+    	}
+    }
+	
+	private boolean buscarDados(int id_atleta){
 		try{
 			cursor = bancoDados.query("premios", 
 					new String [] {"id","id_atleta","nome","tipo"}, 
-					"id_atleta = 1",//selection,
+					"id_atleta = "+Integer.toString(id_atleta),//selection,
 					null,//selectionArgs, 
 					null,//groupBy, 
 					null,//having,
@@ -280,10 +382,93 @@ public class Premios extends Activity {
 								@Override
 								public void onClick(DialogInterface arg0, int arg1) {
 									mdAux.setVisibility(View.GONE);
+									
+									int pos = 0;
+									ImageView ivAux = null;
+									
+									//Corrige o índice (tag) das medalhas
+									for(int i = 0;i<ivArray.size();i++){
+										ivAux = (ImageView) ivArray.get(i);
+										if(ivAux.getTag()==mdAux.getTag()){
+											pos=i;
+											//exibirMensagem("Tag",Integer.toString((Integer)ivAux.getTag()));
+											for(int j = pos+1;j<ivArray.size();j++){
+												ivAux = (ImageView) ivArray.get(j);
+												ivAux.setTag((
+														(Integer)ivAux.getTag())-1);
+											}
+											break;
+										}
+									}
+									
 									bancoDados.delete("premios", 
 											"id = "+Integer.toString(cursor.getInt(
 													cursor.getColumnIndex("id"))), 
 											null);
+									buscarDados(spnArrayId.get(index));
+									if(count<=7){
+										tbRow = (TableRow) findViewById(R.id.tbRow1);
+									}
+									if(count<=14&&count>7){
+										if(pos<7){
+											ajustaTabela("2 > 1");
+										}
+										tbRow = (TableRow) findViewById(R.id.tbRow2);
+									}
+									if(count<=21&&count>14){
+										if(pos<7){
+											ajustaTabela("2 > 1");
+											ajustaTabela("3 > 2");
+										}else{
+											if(pos>=7&&pos<14){
+												ajustaTabela("3 > 2");
+											}
+										}
+										tbRow = (TableRow) findViewById(R.id.tbRow3);
+									}
+									if(count<=28&&count>21){
+										if(pos<7){
+											ajustaTabela("2 > 1");
+											ajustaTabela("3 > 2");
+											ajustaTabela("4 > 3");
+										}else{
+											if(pos>=7&&pos<14){
+												ajustaTabela("3 > 2");
+												ajustaTabela("4 > 3");
+											}else{
+												if(pos>=14&&pos<21){
+													ajustaTabela("4 > 3");
+												}
+											}
+										}
+										tbRow = (TableRow) findViewById(R.id.tbRow4);
+									}
+									if(count>28){
+										if(pos<7){
+											ajustaTabela("2 > 1");
+											ajustaTabela("3 > 2");
+											ajustaTabela("4 > 3");
+											ajustaTabela("5 > 4");
+										}else{
+											if(pos>=7&&pos<14){
+												ajustaTabela("3 > 2");
+												ajustaTabela("4 > 3");
+												ajustaTabela("5 > 4");
+											}else{
+												if(pos>=14&&pos<21){
+													ajustaTabela("4 > 3");
+													ajustaTabela("5 > 4");
+												}else{
+													if(pos>=21&&pos<28){
+														ajustaTabela("5 > 4");
+													}
+												}
+											}
+										}
+										tbRow = (TableRow) findViewById(R.id.tbRow5);
+									}
+									ivArray.remove(pos);
+									count--;
 								}
 							});
 				}catch(Exception erro){
@@ -291,6 +476,53 @@ public class Premios extends Activity {
 				}
 			}
 		});
+	}
+	
+	public void ajustaTabela(String troca){
+		//Retira medalha da linha 2 e coloca na 1
+		if(troca.toString().equals("2 > 1")){
+			try{
+				tbRow = (TableRow) findViewById(R.id.tbRow2);
+				tbRow.removeView((View)ivArray.get(7));
+				tbRow = (TableRow) findViewById(R.id.tbRow1);
+				tbRow.addView(ivArray.get(7));
+			}catch(Exception erro){
+				exibirMensagem("Erro", "Erro: "+erro.getMessage());
+			}
+		}
+		//Retira medalha da linha 3 e coloca na 2
+		if(troca.toString().equals("3 > 2")){
+			try{
+				tbRow = (TableRow) findViewById(R.id.tbRow3);
+				tbRow.removeView((View)ivArray.get(14));
+				tbRow = (TableRow) findViewById(R.id.tbRow2);
+				tbRow.addView(ivArray.get(14));
+			}catch(Exception erro){
+				exibirMensagem("Erro", "Erro: "+erro.getMessage());
+			}
+		}
+		//Retira medalha da linha 4 e coloca na 3
+		if(troca.toString().equals("4 > 3")){
+			try{
+				tbRow = (TableRow) findViewById(R.id.tbRow4);
+				tbRow.removeView((View)ivArray.get(21));
+				tbRow = (TableRow) findViewById(R.id.tbRow3);
+				tbRow.addView(ivArray.get(21));
+			}catch(Exception erro){
+				exibirMensagem("Erro", "Erro: "+erro.getMessage());
+			}
+		}
+		//Retira medalha da linha 5 e coloca na 4
+		if(troca.toString().equals("5 > 4")){
+			try{
+				tbRow = (TableRow) findViewById(R.id.tbRow5);
+				tbRow.removeView((View)ivArray.get(28));
+				tbRow = (TableRow) findViewById(R.id.tbRow4);
+				tbRow.addView(ivArray.get(28));
+			}catch(Exception erro){
+				exibirMensagem("Erro", "Erro: "+erro.getMessage());
+			}
+		}
 	}
 	
 }
